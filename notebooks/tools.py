ed9dbd6e496d1cd45ed6e349087952a23720315f
Your Name <enton>
2024-05-26 17:41:55 -0500

    asdf

--- notebooks/tools.py
+++ notebooks/tools.py
@@ -1,101 +1,93 @@
+from IPython.core.display import HTML
+from IPython.display import display, clear_output
+
+display(HTML("<style>.output_subarea { overflow: auto; }</style>"))
+
+import datetime
+import random
 import os
-from supabase import create_client, Client
+import pandas as pd
+import socket
 
-url: str = "https://dbcizmxdlufqncxipqwt.supabase.co"
-key: str = (
-    "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiY2l6bXhkbHVmcW5jeGlwcXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4NDkxMTMsImV4cCI6MjAxNzQyNTExM30.ys98bhleekrfDJAbrMMqXGsVh1XMa3Vtl8O62s7D5as"
-)
-supabase: Client = create_client(url, key)
+from ib_async import *
 
+hostname = socket.gethostname()
+util.logToFile(f"{datetime.datetime.now().strftime('%Y-%m-%d')}-{hostname}.log")
+util.startLoop()
+util.logToConsole()
 
-if os.name != "posix":
-    import winsound
+if "ib" in globals():
+    ib.disconnect()
 
+randint = lambda a=1, b=10: random.randint(a, b)
+ib = IB()
+ib.connect("127.0.0.1", 4001, randint(1, 99))
 
-def play_beep(freq=2500, dur=500):
-    # On Mac, use the "afplay" command to play a beep sound
-    if os.name == "posix":
-        os.system("afplay /System/Library/Sounds/Glass.aiff")
-    else:
-        winsound.Beep(freq, dur)
-
-
-def import_trades(trades):
-    for t in trades:
-        print(t)
-        d = {
-            "contract_sectype": t.contract.secType,
-            "contract_conid": int(t.contract.conId),
-            "contract_symbol": t.contract.symbol,
-            "contract_lasttradedateorcontractmonth": t.contract.lastTradeDateOrContractMonth,
-            "contract_multiplier": int("1" or t.contract.multiplier),
-            "contract_currency": t.contract.currency,
-            "contract_localsymbol": t.contract.localSymbol,
-            "contract_tradingclass": t.contract.tradingClass,
-            "order_permid": int(t.order.permId),
-            "order_action": t.order.action,
-            "order_ordertype": t.order.orderType,
-            "order_lmtprice": float(t.order.lmtPrice),
-            "order_tif": t.order.tif,
-            "order_ocatype": t.order.ocaType,
-            "order_account": t.order.account,
-            "order_autocanceldate": t.order.autoCancelDate,
-            "order_filledquantity": float(t.order.filledQuantity),
-            "order_reffuturesconid": int(t.order.refFuturesConId),
-            "orderstatus": t.orderStatus.status,
-        }
-
-        data, count = supabase.table("trades").upsert(d).execute()
-
-    print(f"Imported {len(trades)} trades")
+# from supabase import create_client, Client
+# connect to supabase server
+# url: str = "https://dbcizmxdlufqncxipqwt.supabase.co"
+# key: str = (
+#     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRiY2l6bXhkbHVmcW5jeGlwcXd0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3MDE4NDkxMTMsImV4cCI6MjAxNzQyNTExM30.ys98bhleekrfDJAbrMMqXGsVh1XMa3Vtl8O62s7D5as"
+# )
+# supabase: Client = create_client(url, key)
 
 
-def get_all_openorders(ib, sym="NQ"):
-    trades = ib.reqAllOpenOrders()
-    trades.sort(key=lambda trade: trade.order.lmtPrice)
-
-    trades = [trade for trade in trades if trade.contract.symbol == sym]
-    return trades
+import platform
+import chime
 
 
-def print_cancelled_orders(ib):
-    # canceled orders
-    print(f"permId\t\taction\t\ttotalQuantity\tfilledQuantity\t\tlmtPrice")
+def chime_success():
+    if platform.system() == "Linux":
+        chime.success()
+    elif platform.system() == "Darwin":
+        os.system("say beep")
+    elif platform.system() == "Windows":
+        raise Exception("not handled yet")
 
-    closedOrders = ib.orders()
-    [
-        print(
-            f"{order.permId}\t{order.action}\t\t{order.totalQuantity}\t\t{order.filledQuantity}\t\t\t{order.lmtPrice}"
+
+import urllib
+
+
+def push_notifications(msg="Hello world!"):
+    try:
+        body = f"[{datetime.datetime.now()}] {msg}"
+        print(body)
+        data = urllib.parse.urlencode({"text": body}).encode()
+        req = urllib.request.Request(
+            "https://api.chanify.net/v1/sender/CICswLUGEiJBQUZIR0pJQ0VVNkxUTlZCMk1DRElCWU1RSlNWMktCS0NFIgIIAQ.vj8gcfxM4jD9Zv0mBMSlFlY51EL_jC5dB8LWdWX1tAs",
+            data=data,
         )
-        for order in closedOrders
-        if order.filledQuantity == 0
-    ]
+        response = urllib.request.urlopen(req)
+        response.read()  # Read the response to ensure the request is complete
+    except urllib.error.URLError as e:
+        print(f"Error sending request: {e.reason}")
+    except Exception as e:
+        print(f"An unexpected error occurred: {e}")
+
+
+def print_reqOpenOrders():
+    print("Session Orders::")
+    display(util.df([t.order for t in ib.reqOpenOrders()]))
+
+
+def print_reqAllOpenOrders():
+    print("All Session Orders:")
+    display(util.df([t.order for t in ib.reqAllOpenOrders()]))
 
 
 def print_account_summary(ib):
+    print("ACCOUNT SUMMARY::\n")
     acct_fields = ib.accountSummary(account="U10394496")
 
     for f in acct_fields:
         if "DayTrades" not in f.tag:
             print(f.tag, ":", f.value)
-
-
-def print_trades(ib, trades):
-
-    trades.sort(key=lambda trade: trade.order.lmtPrice)
-
-    for trade in trades:
-        orderstatus = trade.orderStatus
-        order = trade.order
-
-        print(
-            f"{trade.contract.symbol}\t{order.permId}\t{orderstatus.status}\t\t{order.action}\t{orderstatus.filled}\t{orderstatus.remaining}\t\t{order.lmtPrice}\t"
-        )
+    print()
 
 
 def print_order(o):
     if o is None:
-        print(o)
+        print()
         return
 
     order = o.order
@@ -109,5 +101,37 @@ def print_order(o):
     )
 
 
-def push_msg():
+def print_executions():
+    fills = [t.fills for t in ib.trades() if t.fills != []]
+    executions = [f[0].execution for f in fills]
+    print(f"Intraday Executions: {len(executions)}\n")
+
+
+def print_positions():
+    print("POSITIONS::")
+    future = [pos for pos in ib.positions()]
+    for f in future:
+        print(
+            f"{f.contract.symbol} {f.position} @ {f.avgCost/float(contract.multiplier)}"
+        )
     print()
+
+
+def print_openOrders(status_list=["Submitted", "PendingSubmit", "PendingCancel"]):
+    print(":: openOrders ::")
+    display(util.df([t for t in ib.openOrders()]))
+
+
+def print_submittedtrades():
+    print(":: trades ::")
+    display(
+        util.df(
+            [
+                t.order
+                for t in ib.trades()
+                if t.orderStatus.status == "Submitted"
+                or t.orderStatus.status == "PreSubmitted"
+                or t.orderStatus.status == "PendingSubmit"
+            ]
+        )
+    )
