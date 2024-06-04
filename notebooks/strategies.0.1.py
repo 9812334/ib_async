d9f06ceb62ae9134be73fe4af47a4813b815d095
Your Name <enton>
2024-06-04 11:51:38 -0500

    asdf

--- notebooks/strategies.0.1.py
+++ notebooks/strategies.0.1.py
@@ -263,8 +263,8 @@ DEBUG = False
 LIVE = True
 
 if __name__ == "__main__":
-    open_permid = 1903109041 # get_last_trade_permid(n=0)
+    open_permid = None # get_last_trade_permid(n=0)
     close_permid = None # get_last_trade_permid()
     print(open_permid, ";", close_permid)
 
-    run_ss_2(strategy_details=SELL_SCALP, open_permid=open_permid, close_permid = close_permid, push = True)
+    run_ss_2(strategy_details=BUY_SCALP, open_permid=open_permid, close_permid = close_permid, push = True)
